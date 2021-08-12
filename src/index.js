397f7d11c17a8033516319a8634f538c89e890d1
谭超 <13996694954@139.com>
2021-08-12 10:29:10 +0800

    核心

--- src/index.js
+++ src/index.js
@@ -1,27 +1,34 @@
+//核心概念 5. State & 生命周期
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-import * as serviceWorker from './serviceWorker';
-//import { timingSafeEqual } from 'crypto';
-class MyComponent extends React.Component {
-    handleClick() {
-        // 使用原生的 DOM API 获取焦点
-        this.refs.myInput.focus();
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { date: new Date() };
+    }
+    componentDidMount() {
+        this.timerID = setInterval(() => this.tick(),);
+    }
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+    tick() {
+        this.setState({
+            date: new Date()
+        });
     }
     render() {
-        //  当组件插入到 DOM 后，ref 属性添加一个组件的引用于到 this.refs
         return (
             <div>
-                <input type="text" ref="myInput" />
-                <input type="button" value="点我输入框获取焦点" onClick={this.handleClick.bind(this)} />
+                <h1>hello,world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
             </div>
-        );
+        )
     }
 }
 
 ReactDOM.render(
-    <MyComponent />,
-    document.getElementById('example')
+    <Clock />,
+    document.getElementById('root')
 );
-serviceWorker.unregister();
